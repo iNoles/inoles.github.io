78c277902dd3e1d83dd37f6eb3c5266f0071cb25
Jonathan Steele <iNoles@users.noreply.github.com>
2024-01-10 20:56:13 -0500

    fixed some warnings

--- README.md
+++ README.md
@@ -1,37 +1,35 @@
+<h1 align="center">
+  Gatsby Minimal TypeScript Starter
+</h1>
 <p align="center">
   <a href="https://www.gatsbyjs.com/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter-ts">
     <img alt="Gatsby" src="https://www.gatsbyjs.com/Gatsby-Monogram.svg" width="60" />
   </a>
 </p>
-<h1 align="center">
-  Gatsby Minimal TypeScript Starter
-</h1>
 
 ## 🚀 Quick start
 
 1.  **Create a Gatsby site.**
 
-    Use the Gatsby CLI to create a new site, specifying the minimal TypeScript starter.
+  Use the Gatsby CLI to create a new site, specifying the minimal TypeScript starter.
 
-    ```shell
-    # create a new Gatsby site using the minimal TypeScript starter
-    npm init gatsby -- -ts
-    ```
+  ```shell
+  # create a new Gatsby site using the minimal TypeScript starter
+  npm init gatsby -- -ts
+  ```
 
 2.  **Start developing.**
-
-    Navigate into your new site’s directory and start it up.
-
-    ```shell
-    cd my-gatsby-site/
-    npm run develop
-    ```
+  
+  Navigate into your new site’s directory and start it up.
+  ```shell
+  cd my-gatsby-site/
+  npm run develop
+  ```
 
 3.  **Open the code and start customizing!**
-
-    Your site is now running at http://localhost:8000!
-
-    Edit `src/pages/index.tsx` to see your site update in real-time!
+ 
+ Your site is now running at `http://localhost:8000!`
+ Edit `src/pages/index.tsx` to see your site update in real-time!
 
 4.  **Learn more**
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,37 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://inoles.github.io`,
+    siteUrl: 'https://inoles.github.io',
     // Your Name
     name: 'Jonathan Steele',
     // Main Site Title
-    title: `Jonathan Steele | Software Developer`,
+    title: 'Jonathan Steele | Software Developer',
     // Description that goes under your name in main bio
-    description: `Software Developer from Florida`,
+    description: 'Software Developer from Florida',
     // Optional: Github account URL
-    github: `https://github.com/inoles`,
+    github: 'https://github.com/inoles',
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/jonathan-steele`,
+    linkedin: 'https://www.linkedin.com/in/jonathan-steele',
     // Content of the About Me section
-    about: `Experienced software developer with a 4.7-star app store rating on iOS and Android. Improved app performance by 30% and reduced project timelines by 15%. Enhanced team productivity by 20% with Python scripting. Committed to achieving results.`
+    about: 'Experienced software developer with a 4.7-star app store rating on iOS and Android. Improved app performance by 30% and reduced project timelines by 15%. Enhanced team productivity by 20% with Python scripting. Committed to achieving results.'
   },
   plugins: [
-    "gatsby-plugin-postcss",
-    `gatsby-transformer-yaml`,
+    'gatsby-plugin-postcss',
+    'gatsby-transformer-yaml',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/`
       }
     },
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-CVE0ZBT5L4", // Google Analytics / GA
-        ],
+          'G-CVE0ZBT5L4' // Google Analytics / GA
+        ]
       }
-    },
+    }
   ]
-};
+}
