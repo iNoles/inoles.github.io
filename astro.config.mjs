0caef063bf713590e1a541d1598a276c17f8c38a
Jonathan Steele <iNoles@users.noreply.github.com>
2024-12-15 20:05:36 -0500

    Upgrade to Tailwind CSS v4 Beta with additonal changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   vite: {
-    plugins: [yaml()]
+    plugins: [yaml(), tailwindcss()]
   },
   integrations: [
-    tailwind({
-      // Example: Allow writing nested CSS declarations
-      // alongside Tailwind's syntax
-      nesting: true,
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
-    }),
     partytown({ config: { forward: ['dataLayer.push'] } }),
   ],
 });
