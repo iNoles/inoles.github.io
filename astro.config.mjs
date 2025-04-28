8a692934d07adeeb88d862eba1ecf24d59358cdd
Jonathan Steele <iNoles@users.noreply.github.com>
2025-04-28 18:05:40 -0400

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 import tailwindcss from '@tailwindcss/vite';
 import partytown from '@astrojs/partytown';
@@ -7,6 +7,13 @@ import partytown from '@astrojs/partytown';
 export default defineConfig({
   vite: {
     plugins: [yaml(), tailwindcss()]
+  },
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter"
+    }]
   },
   integrations: [
     partytown({ config: { forward: ['dataLayer.push'] } }),
